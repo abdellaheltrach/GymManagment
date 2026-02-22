e6b995ece1e41acc105698e6c2fac9f7a3ef0144
abdellaheltrach <abdellah.eltrach@gmail.com>
2026-02-22 23:23:51 +0000

    install pack Microsoft.AspNetCore.Identity.EntityFrameworkCore

---
+++ GymManagement.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace GymManagement.Domain.Entities.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName => $"{FirstName} {LastName}";
+
+        public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+        public DateTime? LastLoginAt { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
